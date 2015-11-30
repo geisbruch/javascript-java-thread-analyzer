417dc41b7f2decf0b176b63644227164ef4ceaff
geisbruch <gabrieleisbruch@gmail.com>
2015-11-30 12:01:11 -0300

    Initial Commit

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,15 @@ var gutil = require('gulp-util');
 gulp.task('build', function () {
   // set up the browserify instance on a task basis
   var b = browserify({
-    entries: './entry.js',
+    entries: './main.js',
     debug: true
   });
 
   return b.bundle()
-    .pipe(source('main.js'))
+    .pipe(source('java-thread-analyzer.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here.
-        .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/js/'));
+        // Add transformation tasks to the pipeline here.
+        
 });
