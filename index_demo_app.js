96cd708c1b1f44b59e99a100a0ab220325e9ac70
geisbruch <gabrieleisbruch@gmail.com>
2015-12-04 16:45:44 -0300

    Add sitemap

--- index_demo_app.js
+++ index_demo_app.js
@@ -9,6 +9,10 @@ app.get("/robots.txt",function(req, res) {
   res.sendFile(__dirname+'/static/robots.txt');  
 });
 
+app.get("/sitemap.xml",function(req, res) {
+  res.sendFile(__dirname+'/static/sitemap.xml');  
+});
+
 app.get("/",function(req, res) {
   res.sendFile(__dirname+'/static/index.html');  
 });
